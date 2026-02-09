49f8e9b22b1b016053deaa4e79619af68d547378
michel_32 <karas.benjamin@gmail.com>
2026-02-09 19:05:03 +0100

    update + comment calculateArea()

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,18 +6,25 @@
 /*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 17:33:30 by michel_32         #+#    #+#             */
-/*   Updated: 2026/02/09 18:51:25 by michel_32        ###   ########.fr       */
+/*   Updated: 2026/02/09 19:04:45 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
+/*
+The result sign depends on whether vertices are clockwise (negative) 
+or counter-clockwise (positive).
+Return absolute value to get the geometric area magnitude
+* 
+*/
 static Fixed calculateArea(Point const a, Point const b, Point const c)
 {
-    // The result depends on whether vertices are clockwise or counter-clockwise
     Fixed res = (a.getX() * (b.getY() - c.getY()) +
                  b.getX() * (c.getY() - a.getY()) +
                  c.getX() * (a.getY() - b.getY())) / 2;
-    // Return absolute value to get the geometric area magnitude
-    return (res < 0) ? res * -1 : res;
+    if (res < 0)
+        return (res * -1);
+    else
+        return (res);
 }

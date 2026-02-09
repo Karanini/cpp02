2c5594d9847a3bb831a22be097defc5dd9ae13d7
michel_32 <karas.benjamin@gmail.com>
2026-02-09 18:06:14 +0100

    add operator=()

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 17:33:11 by michel_32         #+#    #+#             */
-/*   Updated: 2026/02/09 17:46:12 by michel_32        ###   ########.fr       */
+/*   Updated: 2026/02/09 18:05:57 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,15 @@ Point::Point(const float num_x, const float num_y) : _x(num_x), _y(num_y)
 Point::Point(const Point &point)
 {
     *this = point;
+}
+
+/*
+* this function is required in the OCF, but since `_x` and `_y` are const, 
+* they cannot be reassigned anymore. So we `(void) point` to avoid 
+* compiler warnings and we simply return `*this`
+*/
+Point   &Point::operator=(const Point &point)
+{
+    (void) point;
+    return (*this);
 }

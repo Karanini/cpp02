f56cbfb9228325c3589812cd5f5dffdfef052cab
michel_32 <karas.benjamin@gmail.com>
2026-02-09 17:46:33 +0100

    add constructors

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,8 +6,23 @@
 /*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 17:33:11 by michel_32         #+#    #+#             */
-/*   Updated: 2026/02/09 17:33:25 by michel_32        ###   ########.fr       */
+/*   Updated: 2026/02/09 17:46:12 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
+
+Point::Point() : _x(0), _y(0)
+{
+
+}
+
+Point::Point(const float num_x, const float num_y) : _x(num_x), _y(num_y)
+{
+
+}
+
+Point::Point(const Point &point)
+{
+    *this = point;
+}

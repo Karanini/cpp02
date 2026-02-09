bafa9670de9b51a4571f1a30914495130ba614d9
michel_32 <karas.benjamin@gmail.com>
2026-02-09 17:42:13 +0100

    create required files + add OCF functions declarations in Point.hpp

---
+++ ex03/Point.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/09 17:32:56 by michel_32         #+#    #+#             */
+/*   Updated: 2026/02/09 17:41:13 by michel_32        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef POINT_HPP
+#define POINT_HPP
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+class Point
+{
+private:
+    Fixed const _x;
+    Fixed const _y;
+public:
+    Point();
+    Point(const float num_x, const float num_y);
+    Point(const Point &point);
+    Point   &operator=(const Point &point);
+    ~Point();
+};
+
+bool bsp( Point const a, Point const b, Point const c, Point const point);
+
+
+#endif
